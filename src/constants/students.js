4423ea8a2f50b043f8cf21cde612438d72e34f06
unknown <abu040810@gmail.com>
2024-10-24 15:04:40 +0500

    finishing

---
+++ src/constants/students.js
@@ -0,0 +1,52 @@
+export const students = [
+    {
+      id: 1,
+      name: 'John',
+      surname: 'Doe',
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6v-Quj0rUbKfkYkO5xry7QsyV_3dNemjlbw&usqp=CAU',
+      group_number: '122',
+      grade: 'Freelance',
+      createdAt: '2023.04.01',
+      id_number: '1234'
+    },
+    {
+      id: 2,
+      name: 'Jesse',
+      surname: 'Lingard',
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6v-Quj0rUbKfkYkO5xry7QsyV_3dNemjlbw&usqp=CAU',
+      group_number: '122',
+      grade: 'Freelance',
+      createdAt: '2023.04.01',
+      id_number: '1234'
+    },
+    {
+      id: 3,
+      name: 'Lionel',
+      surname: 'Neymar',
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6v-Quj0rUbKfkYkO5xry7QsyV_3dNemjlbw&usqp=CAU',
+      group_number: '122',
+      grade: 'Freelance',
+      createdAt: '2023.04.01',
+      id_number: '1234'
+    },
+    {
+      id: 4,
+      name: 'Klyan',
+      surname: 'Haaland',
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6v-Quj0rUbKfkYkO5xry7QsyV_3dNemjlbw&usqp=CAU',
+      group_number: '122',
+      grade: 'Freelance',
+      createdAt: '2023.04.01',
+      id_number: '1234'
+    },
+    {
+      id: 5,
+      name: 'Adam',
+      surname: 'Johnson',
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6v-Quj0rUbKfkYkO5xry7QsyV_3dNemjlbw&usqp=CAU',
+      group_number: '122',
+      grade: 'Freelance',
+      createdAt: '2023.04.01',
+      id_number: '1234'
+    }
+  ]
---
+++ src/constants/teacher.js
@@ -0,0 +1,90 @@
+export const teachers = [
+    {
+        id: 1,
+        name: 'John',
+        surname: 'Doe',
+        subject: 'Mathematics',
+        img: 'https://eduenglishpassport.sru.ac.th/assets/images/users/1.jpg',
+        description: 'Im math teacher',
+        login: 'login',
+        password: 'password',
+        createdAt: '2023.05.18'
+    },
+    {
+        id: 2,
+        name: 'John',
+        surname: 'Doe',
+        subject: 'Mathematics',
+        img: 'https://eduenglishpassport.sru.ac.th/assets/images/users/1.jpg',
+        description: 'Im math teacher',
+        login: 'login',
+        password: 'password',
+        createdAt: '2023.05.18'
+    },
+    {
+        id: 3,
+        name: 'John',
+        surname: 'Doe',
+        subject: 'Mathematics',
+        img: 'https://eduenglishpassport.sru.ac.th/assets/images/users/1.jpg',
+        description: 'Im math teacher',
+        login: 'login',
+        password: 'password',
+        createdAt: '2023.05.18'
+    },
+    {
+        id: 4,
+        name: 'John',
+        surname: 'Doe',
+        subject: 'Mathematics',
+        img: 'https://eduenglishpassport.sru.ac.th/assets/images/users/1.jpg',
+        description: 'Im math teacher',
+        login: 'login',
+        password: 'password',
+        createdAt: '2023.05.18'
+    },
+    {
+        id: 5,
+        name: 'John',
+        surname: 'Doe',
+        subject: 'Mathematics',
+        img: 'https://eduenglishpassport.sru.ac.th/assets/images/users/1.jpg',
+        description: 'Im math teacher',
+        login: 'login',
+        password: 'password',
+        createdAt: '2023.05.18'
+    },
+    {
+        id: 6,
+        name: 'John',
+        surname: 'Doe',
+        subject: 'Mathematics',
+        img: 'https://eduenglishpassport.sru.ac.th/assets/images/users/1.jpg',
+        description: 'Im math teacher',
+        login: 'login',
+        password: 'password',
+        createdAt: '2023.05.18'
+    },
+    {
+        id: 7,
+        name: 'John',
+        surname: 'Doe',
+        subject: 'Mathematics',
+        img: 'https://eduenglishpassport.sru.ac.th/assets/images/users/1.jpg',
+        description: 'Im math teacher',
+        login: 'login',
+        password: 'password',
+        createdAt: '2023.05.18'
+    },
+    {
+        id: 8,
+        name: 'John',
+        surname: 'Doe',
+        subject: 'Mathematics',
+        img: 'https://eduenglishpassport.sru.ac.th/assets/images/users/1.jpg',
+        description: 'Im math teacher',
+        login: 'login',
+        password: 'password',
+        createdAt: '2023.05.18'
+    },
+]
